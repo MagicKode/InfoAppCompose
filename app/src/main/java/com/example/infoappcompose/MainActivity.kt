0607edf554ad58aab9938283995ec2078247d2f0
dmi6756@yandex.ru <a0zvx753>
2024-10-31 17:20:06 +0300

    First commit

--- app/src/main/java/com/example/infoappcompose/MainActivity.kt
+++ app/src/main/java/com/example/infoappcompose/MainActivity.kt
@@ -1,22 +1,8 @@
 package com.example.infoappcompose
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import com.example.infoappcompose.ui.theme.InfoAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,49 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             InfoAppComposeTheme {
-                MainScreen(this)
             }
         }
-    }
-}
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun MainScreen(context: Context) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Menu")
-                },
-                backgroundColor = Color.White,
-                navigationIcon = {
-                    IconButton(
-                        onClick = {
-                            Toast.makeText(context, "Menu", Toast.LENGTH_SHORT).show()
-                        }
-                    ) {
-                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {
-                        Toast.makeText(context, "Delete", Toast.LENGTH_SHORT).show()
-                    }
-                    ) {
-                        Icon(imageVector = Icons.Filled.Delete, contentDescription = "Delete")
-                    }
-
-                    IconButton(onClick = {
-                        Toast.makeText(context, "Share", Toast.LENGTH_SHORT).show()
-                    }
-                    ) {
-                        Icon(imageVector = Icons.Filled.Share, contentDescription = "Share")
-                    }
-                }
-            )
-        }
-    ) {
-        it
     }
 }
