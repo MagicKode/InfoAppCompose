b9468b2e55edfc54292f3e61ee49bd268253d0de
dmi6756@yandex.ru <a0zvx753>
2024-11-01 16:14:05 +0300

    1. Создание верхней Панели с кнопками 2. Добавил кнопку Меню 3. Сделал, выдвижное меню со списком кнопок категорий продуктов. 4. Добавил кнопку Избранные

--- app/src/main/java/com/example/infoappcompose/MainActivity.kt
+++ app/src/main/java/com/example/infoappcompose/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.infoappcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.infoappcompose.ui.theme.InfoAppComposeTheme
+import com.example.infoappcompose.ui_components.DriverMenu
+import com.example.infoappcompose.ui_components.MainTopBar
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            val topBarTitle = remember {
+                mutableStateOf("Продукты")  //пока хардкодим наименование загаловка шапки
+            }
             InfoAppComposeTheme {
+                Scaffold(      //создаём контейнер для создания шапки и кнопок приложения
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        MainTopBar(
+                            title = topBarTitle.value,
+                            scaffoldState
+                        )
+                    },
+                    drawerContent = {
+                        DriverMenu()
+                    }
+                ) {
+                    it
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/infoappcompose/ui_components/MainTopBar.kt
@@ -0,0 +1,56 @@
+package com.example.infoappcompose.ui_components
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.launch
+
+
+/**
+ * Создаём шапку с кнопками меню и остальными
+ */
+@Composable
+fun MainTopBar(title: String, scaffoldState: ScaffoldState) {
+    val coroutine = rememberCoroutineScope()
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        backgroundColor = Color.White,
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    coroutine.launch {
+                        scaffoldState.drawerState.open()   //чтобы открывалось меню, необходимо использовать Coroutine
+                    }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Menu"
+                )
+            }
+        }
+    )
+
+}
