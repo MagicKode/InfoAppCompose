ba076d7d14928c3fb23972d0b4fbff355e2a9826
dmi6756@yandex.ru <a0zvx753>
2024-11-04 16:57:11 +0300

    1. Создание экрана заставки запуска приолжения

--- app/src/main/java/com/example/infoappcompose/MainActivity.kt
+++ app/src/main/java/com/example/infoappcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,12 +14,23 @@ import com.example.infoappcompose.ui_components.MainScreen
 import com.example.infoappcompose.utils.Routes
 import com.example.infoappcompose.utils.ListItem
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        installSplashScreen()
+//        CoroutineScope(Dispatchers.Main).launch {
+//            delay(2000L)
+//            splashScreen.setKeepOnScreenCondition {false}
+//        }
+
         setContent {
             val navController = rememberNavController()  // ДОБАВЛЯЕМ НАВИГАЦИЮ! Для переключения экранов
             var item: ListItem? = null
